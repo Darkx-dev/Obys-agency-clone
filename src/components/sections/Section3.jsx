a292e42cdbaf7630d4952f2a5fdd367639204593
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-04-15 00:11:07 +0530

    All Showcase Elements Completed

--- src/components/sections/Section3.jsx
+++ src/components/sections/Section3.jsx
@@ -2,9 +2,9 @@ import ProjectCard from "../ProjectCard";
 import olga from "/src/assets/Olga_Prudka_1.png";
 import aim from "/src/assets/AIM-1.png";
 import Arrow from "../Arrow";
-import ochi from '/src/assets/OCHI.png'
-import laxer from "/src/assets/Laxer_1-2.png"
-import eminente from '/src/assets/Eminente_First.png'
+import ochi from "/src/assets/OCHI.png";
+import laxer from "/src/assets/Laxer_1-2.png";
+import eminente from "/src/assets/Eminente_First.png";
 function Section3() {
   return (
     <section data-scroll-section>
@@ -38,8 +38,8 @@ function Section3() {
             projectDescription={"Logo Design, Website Design, Development"}
             projectDate={2024}
           />
-          <Arrow justify={"end"} degree={0}/>
-          <Arrow justify={"end"} degree={45}/>
+          <Arrow justify={"end"} degree={0} />
+          <Arrow justify={"end"} degree={45} />
           <ProjectCard
             projectTitle={"OCHI"}
             projectImage={ochi}
@@ -56,11 +56,18 @@ function Section3() {
           <ProjectCard
             projectTitle={"ÉMINENTE"}
             projectImage={eminente}
-            projectDescription={"Logo Design, <br/>Website Design, <br/>Development"}
+            projectDescription={"Logo Design, Website Design, Development"}
             projectDate={2023}
           />
-         
+          <Arrow justify={"end"} degree={45} />
+          <ProjectCard
+            projectTitle={"MAKHNO"}
+            projectImage={eminente}
+            projectDescription={"Logo Design, Website Design, Development"}
+            projectDate={2023}
+          />
         </div>
+        
       </div>
     </section>
   );
