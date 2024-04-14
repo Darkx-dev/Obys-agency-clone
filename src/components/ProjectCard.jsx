23483f1bed62c447e1ab2a89fc3dfa143642de25
Darkx23312 <130650239+Darkx-dev@users.noreply.github.com>
2024-04-14 23:32:36 +0530

    fixed grid layout (gap + row heights) , Bugs in passing props

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,28 +1,44 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-
-function ProjectCard(
-  { projectImage, projectTitle, projectDescription, projectDate }
-) {
+function ProjectCard({
+  projectImage,
+  projectTitle,
+  projectDescription,
+  projectDate,
+  ytranslate,
+}) {
   return (
-    <div data-scroll data-scroll-speed="1" className="project-card">
-      <h2 className="text-[2.8rem] text-nowrap">{projectTitle}</h2>
-      <div className="project-image-container">
-        <img src={projectImage} alt="Some Image" />
+    {
+      projectImage,
+      projectTitle,
+      projectDescription,
+      projectDate,
+      ytranslate,
+    } && (
+      <div
+        data-scroll
+        data-scroll-speed="1"
+        className={`project-card translate-y-${ytranslate} `}
+      >
+        <h2 className="text-[2.8rem] text-nowrap">{projectTitle}</h2>
+        <div className="project-image-container">
+          <img src={projectImage} alt="Some Image" />
+        </div>
+        <div className="project-des-date-container grid grid-cols-2 leading-tight text-[1rem] py-10 border-b-2 font-[400]">
+          <p className="">{projectDescription}</p>
+          <p className="justify-self-end">{projectDate}</p>
+        </div>
       </div>
-      <div className="project-des-date-container grid grid-cols-2 leading-tight text-[1rem] py-10 border-b-2 font-[400]">
-        <p className="">{projectDescription}</p>
-        <p className="justify-self-end">{projectDate}</p>
-      </div>
-    </div>
+    )
   );
 }
 
 ProjectCard.propTypes = {
-    projectImage: PropTypes.string.isRequired,
-    projectTitle: PropTypes.string.isRequired,
-    projectDescription: PropTypes.string.isRequired,
-    projectDate: PropTypes.string.isRequired,
-  };
+  projectImage: PropTypes.string,
+  projectTitle: PropTypes.string,
+  projectDescription: PropTypes.string,
+  projectDate: PropTypes.number,
+  ytranslate: PropTypes.number,
+};
 
 export default ProjectCard;
--- src/components/sections/Section3.jsx
+++ src/components/sections/Section3.jsx
@@ -2,11 +2,14 @@ import ProjectCard from "../ProjectCard";
 import olga from "/src/assets/Olga_Prudka_1.png";
 import aim from "/src/assets/AIM-1.png";
 import Arrow from "../Arrow";
+import ochi from '/src/assets/OCHI.png'
+import laxer from "/src/assets/Laxer_1-2.png"
+import eminente from '/src/assets/Eminente_First.png'
 function Section3() {
   return (
     <section data-scroll-section>
       <div className="flex flex-col justify-center text-[7rem] font-semibold leading-[6.8rem]">
-        <div className="top-container mt-32 mb-10 flex justify-center gap-20">
+        <div className="top-container mt-32 mb-10 flex justify-center gap-20 ">
           <div
             data-scroll
             data-scroll-speed="-2"
@@ -17,7 +20,7 @@ function Section3() {
           <div
             data-scroll
             data-scroll-speed="-2"
-            className="right-container z-10"
+            className="right-container z-10 border-b-2 pb-20"
           >
             <h1>OUR PROJECTS</h1>
           </div>
@@ -26,16 +29,37 @@ function Section3() {
           <ProjectCard
             projectTitle={"OLGA PRUDKA"}
             projectImage={olga}
-            projectDescription={"Logo Design,Website Design and Development"}
+            projectDescription={"Logo Design, Website Design, Development"}
             projectDate={2023}
           />
           <ProjectCard
             projectTitle={"AIM"}
             projectImage={aim}
-            projectDescription={"Logo Design,Website Design and Development"}
+            projectDescription={"Logo Design, Website Design, Development"}
+            projectDate={2024}
+          />
+          <Arrow justify={"end"} degree={0}/>
+          <Arrow justify={"end"} degree={45}/>
+          <ProjectCard
+            projectTitle={"OCHI"}
+            projectImage={ochi}
+            projectDescription={"Website Design, Development"}
+            projectDate={2022}
+            ytranslate={20}
+          />
+          <ProjectCard
+            projectTitle={"David Laxer"}
+            projectImage={laxer}
+            projectDescription={"Website Design, Development"}
             projectDate={2023}
           />
-          <Arrow justify={"end"} degree={"0"}/>
+          <ProjectCard
+            projectTitle={"ÉMINENTE"}
+            projectImage={eminente}
+            projectDescription={"Logo Design, <br/>Website Design, <br/>Development"}
+            projectDate={2023}
+          />
+         
         </div>
       </div>
     </section>
